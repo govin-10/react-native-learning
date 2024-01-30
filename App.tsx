8c4005261ef2e162c30bc10def6d1168f2cc2b3a
govin-10 <govindatimsina@outlook.com>
2024-01-30 20:40:13 +0545

    testing materialtopbar navigation

--- App.tsx
+++ App.tsx
@@ -6,9 +6,13 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text, View, Button} from 'react-native';
+import {SafeAreaView, Text, View, Button, StatusBar} from 'react-native';
 import ButtonComp from './components/ButtonComp';
 import UseState from './components/UseState';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 const UserData: React.FC<{name: string; age: number; email: string}> = ({
   name,
@@ -24,21 +28,34 @@ const UserData: React.FC<{name: string; age: number; email: string}> = ({
   );
 };
 
+const Tab = createMaterialTopTabNavigator();
+
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{marginTop: 40}}>
-      <View>
-        <ButtonComp />
+    // <SafeAreaView style={{marginTop: 40}}>
+    //   <View>
+    //     <StatusBar
+    //       backgroundColor={'#a6c0d9'}
+    //       hidden={false}
+    //       barStyle={'dark-content'}
+    //     />
+    //     <ButtonComp />
 
-        <Text>Hello World</Text>
+    //     <Text>Hello World</Text>
 
-        {/* <UserData name="GOvin" age={34} email="hellogovin@gmail.com" />
-        <UserData name="Tim" age={22} email="timdavid@gmail.com" />
-        <Text>Hello</Text> */}
+    //     {/* <UserData name="GOvin" age={34} email="hellogovin@gmail.com" />
+    //     <UserData name="Tim" age={22} email="timdavid@gmail.com" />
+    //     <Text>Hello</Text> */}
 
-        <UseState />
-      </View>
-    </SafeAreaView>
+    //     <UseState />
+    //   </View>
+    // </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
